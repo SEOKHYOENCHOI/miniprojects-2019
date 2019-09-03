89b61ba2655031d3991a9fd722348de8c5a68e4b
seokhyeonchoi <lkamna12@gmail.com>
2019-09-03 13:30:39 +0900

    refactor: timezone 설정

--- src/main/java/com/wootube/ioi/WootubeApplication.java
+++ src/main/java/com/wootube/ioi/WootubeApplication.java
@@ -4,10 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @PropertySource("classpath:email.properties")
 public class WootubeApplication {
     public static void main(String[] args) {
         SpringApplication.run(WootubeApplication.class, args);
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
     }
 }
